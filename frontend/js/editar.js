e37aacdff0b825c044a158683d0911b321c95470
Juan Esteban Kalocai Scalezi <jeks2002gmail.com>
2026-02-20 21:24:02 -0300

    Actualizacion al editar.js

--- frontend/js/editar.js
+++ frontend/js/editar.js
@@ -1,1 +1,18 @@
 /* Codigo JS necesario para las funcionalidades de editar.html */
+const parametrosURL = new URLSearchParams(window.location.search);
+const idDdeveloper = parametrosURL.get ('id')
+const formulario = document.getElementsByid('Formulario-editar');
+
+
+formulario.addEventListener('submit', async(evento)) =
+    evento.preventDefault();
+
+    const datosNuevos = {
+        name: document.getElementById('nombre').value,
+        skills: document.getElementById('habilidad').value,
+        salary: document.getElementById('salario').value,
+        avatar: document.getElementById('avatar').value,
+    }
+
+
+
